2600412965800e1ff909159bd53f9a07eee71690
paivalucass <lucaspaiva2805@gmail.com>
2024-06-17 14:26:36 -0300

    Add new ethernet directory structure and a small .pcap extension read file

---
+++ src/Ethernet/detection/ethernet_detector.py
@@ -0,0 +1,11 @@
+from pcapfile import savefile
+
+class EthernetDetector:
+    def __init__(self, pcap_file):
+        self.pcap_file = pcap_file
+
+    def detect(self):
+        with open(self.pcap_file, 'rb') as file:
+            pcap = savefile.load_savefile(file, verbose=True)
+            for packet in pcap.packets:
+                pass
